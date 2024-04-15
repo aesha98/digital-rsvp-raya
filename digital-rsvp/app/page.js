969fc2ddc6dde7f6efadd0adf86dc2f737899f02
aesha98 <sityasya@gmail.com>
2024-04-16 00:22:26 +0800

    add grid layout

--- digital-rsvp/app/page.js
+++ digital-rsvp/app/page.js
@@ -4,12 +4,14 @@ import { roboto_mono } from './ui/font';
 export default function Home()
 {
 	return(
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<div className="z-10 max-w-5xl w-full items-center  justify-between  text-sm lg:flex">
+		<main className="relative flex flex-col bg-[url(/grid.svg)] p-10 [mask-image:linear-gradient(180deg, white,rgba(255,255,255,0))] items-center">
+			<div className='relative flex min-h-screen flex-col justify-evenly border overflow-hidden bg-teal-950 py-6 sm:py-12 p-10'>
+			{/* <div className='absolute inset-0  bg-[url(/grid.svg)] bg-center [mask-image:linear-gradient(180deg, white,rgba(255,255,255,0))]'></div> */}
+			<div className="relative z-10 max-w-5xl w-full items-center justify-center  text-sm lg:flex">
 
-				<p className={'font-mono text-2xl fixed left-0 top-0 flex w-full font-italic justify-center bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'}> YOU&apos;RE INVITED</p>
+				<p className={'font-mono text-2xl fixed left-0 top-0 flex w-full font-italic justify-center bg-gradient-to-b from-teal-800 pb-6 pt-8 backdrop-blur-xl dark:border-teal-800 dark:bg-teal-950/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:bg-teal-950 lg:p-4 lg:dark:bg-teal-950/30'}> YOU&apos;RE INVITED</p>
 
-				<div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-teal-950 dark:via-teal-950 lg:static lg:h-auto lg:w-auto lg:bg-none">
+				<div className="mt-10 fixed bottom-0 left-0 flex h-24 w-full items-end justify-center bg-gradient-to-t from-teal-800 via-teal-950 dark:from-teal-950 dark:via-teal-950 lg:static lg:h-auto lg:w-auto lg:bg-none">
 
 				 <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
@@ -24,7 +26,7 @@ export default function Home()
               alt="Vercel Logo"
               className="dark:invert"
               width={100}
-              height={24}
+              height={20}
               priority
             />
           </a>
@@ -33,7 +35,7 @@ export default function Home()
 			</div>
 	
 	{/* Image Family */}
-			 <div className="relative m-2 flex flex-col place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-teal-200 after:via-teal-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-teal-100 before:dark:opacity-10 after:dark:from-teal-950 after:dark:via-[#1e331f] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+			 <div className="relative flex flex-col place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-teal-200 before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-teal-200 after:via-teal-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-teal-100 before:dark:opacity-10 after:dark:from-teal-950 after:dark:via-[#1e331f] after:dark:opacity-40 before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] border rounded"
           src="/digital_rsvp_img.png"
@@ -45,12 +47,12 @@ export default function Home()
         />
 
 			<h2 className="font-sans mt-10 text-sm text-center">We invite you to our open house in occasion in celebrating raya</h2>
-			<br/>
+		
       </div>
 
 		
 		{/* Event Information */}
-		<div className='mt-5 mb-20 flex flex-col text-center lg:max-w-lg lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-center'>
+		<div className='relative flex flex-col text-center lg:max-w-lg lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-center'>
 
 			<h2 className="font-sans mb-3 text-xl text-center font-bold">SUNDAY, 28 APRIL 2024</h2>
 			<p className='font-sans mb-3 text-sm text-center font-bold'>12-5PM</p>
@@ -64,7 +66,7 @@ export default function Home()
 			target='_blank'
 			rel='noopener noreferer'>RSVP NOW</a>
 		</div>
-
+		</div>
 		</main>
 	);
 }
