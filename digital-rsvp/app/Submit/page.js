6f429aea0c79c13db07cd855cd9b8de4e60fd916
aesha98 <sityasya@gmail.com>
2024-04-16 02:07:07 +0800

    update layout

--- digital-rsvp/app/Submit/page.js
+++ digital-rsvp/app/Submit/page.js
@@ -4,9 +4,9 @@ export default function Submit_rsvp()
 {
 	return(
 		<main className="relative flex flex-col bg-[url(/grid.svg)] p-10 [mask-image:linear-gradient(180deg, white,rgba(255,255,255,0))] items-center">
-			<div className='relative flex min-h-screen flex-col justify-evenly border border-yellow-400 overflow-hidden bg-teal-950 py-6 sm:py-12 p-10'>
+			<div className='relative flex min-h-screen flex-col border border-yellow-400 overflow-hidden bg-teal-950 py-6 sm:py-12 p-10'>
 			{/* <div className='absolute inset-0  bg-[url(/grid.svg)] bg-center [mask-image:linear-gradient(180deg, white,rgba(255,255,255,0))]'></div> */}
-			<div className="relative z-10 max-w-5xl w-full items-center justify-center  text-sm lg:flex">
+			<div className="relative z-10 max-w-5xl w-full text-sm lg:flex">
 
 				<p className={'font-mono text-2xl fixed left-0 top-0 flex w-full font-italic justify-center bg-gradient-to-b from-teal-800 pb-6 pt-8 backdrop-blur-xl dark:border-teal-800 dark:bg-teal-950/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:bg-teal-950 lg:p-4 lg:dark:bg-teal-950/30'}> Please RSVP Below</p>
 
@@ -33,16 +33,7 @@ export default function Submit_rsvp()
 				
 			</div>
 	
-	{/* Image Family */}
-			 <div className="relative flex flex-col place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-teal-200 before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-teal-200 after:via-teal-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-teal-100 before:dark:opacity-10 after:dark:from-teal-950 after:dark:via-[#1e331f] after:dark:opacity-40 before:lg:h-[360px]">
-       
-	
-		
-      </div>
-
-		
-
-		<div className='relative flex flex-col text-center lg:max-w-lg lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-center'>
+		<div className='flex flex-col text-center lg:max-w-lg lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-center align-top justify-start'>
 			{/* RSVP Form */}
           <form className="mt-10 w-full flex flex-col items-center justify-center gap-6">
             {/* Name Input */}
--- digital-rsvp/app/page.js
+++ digital-rsvp/app/page.js
@@ -1,70 +1,62 @@
 import Image from 'next/image'
-import { roboto_mono } from './ui/font';
 
-export default function Home()
-{
-	return(
-		<main className="relative flex flex-col bg-[url(/grid.svg)] p-10 [mask-image:linear-gradient(180deg, white,rgba(255,255,255,0))] items-center">
-			<div className='relative flex min-h-screen flex-col justify-evenly border border-yellow-400 overflow-hidden bg-teal-950 py-6 sm:py-12 p-10'>
-			{/* <div className='absolute inset-0  bg-[url(/grid.svg)] bg-center [mask-image:linear-gradient(180deg, white,rgba(255,255,255,0))]'></div> */}
-			<div className="relative z-10 max-w-5xl w-full items-center justify-center  text-sm lg:flex">
+export default function Home() {
+  return (
+    <main className="flex flex-col bg-[url(/grid.svg)] p-10 mask-image-[linear-gradient(180deg,_white,_rgba(255,255,255,0))] items-center">
+      <div className='mt-0 flex min-h-screen flex-col border border-yellow-400 overflow-hidden bg-teal-950 py-12 px-10'>
+        
+        <div className="z-10 w-full text-center">
 
-				<p className={'font-mono text-2xl fixed left-0 top-0 flex w-full font-italic justify-center bg-gradient-to-b from-teal-800 pb-6 pt-8 backdrop-blur-xl dark:border-teal-800 dark:bg-teal-950/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:bg-teal-950 lg:p-4 lg:dark:bg-teal-950/30'}> YOU&apos;RE INVITED</p>
+          <h1 className='text-2xl font-mono font-italic bg-gradient-to-b from-teal-950 to-transparent pb-6 pt-8 backdrop-blur-xl border-b border-yellow-400 dark:bg-teal-950/30 lg:rounded-xl lg:p-4'>
+            YOU'RE INVITED
+          </h1>
 
-				<div className="mt-10 fixed bottom-0 left-0 flex h-24 w-full items-end justify-center bg-gradient-to-t from-teal-800 via-teal-950 dark:from-teal-950 dark:via-teal-950 lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <div className="mt-10 mb-10 flex flex-col items-center justify-center lg:flex-row lg:justify-between">
 
-				 <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-			{/* <a href="https://www.flaticon.com/free-icons/raya" title="raya icons">Raya icons created by LAB Design Studio - Flaticon</a> */}
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={20}
-              priority
-            />
-          </a>
-				</div>
-				
-			</div>
-	
-	{/* Image Family */}
-			 <div className="relative flex flex-col place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-teal-200 before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-teal-200 after:via-teal-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-teal-100 before:dark:opacity-10 after:dark:from-teal-950 after:dark:via-[#1e331f] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] border rounded"
-          src="/digital_rsvp_img.png"
-          alt="Next.js Logo"
-          width={500}
-          height={400}
-		
-          priority
-        />
+            <div className="mb-6 lg:mb-0 lg:flex lg:items-center">
+              <Image
+                src="/digital_rsvp_img.png"
+                alt="RSVP Invitation"
+                width={500}
+                height={400}
+                priority
+                className="rounded border dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
+              />
+            </div>
+            
+            <div>
+              <h2 className="mt-2 mb-10 text-sm">We invite you to our open house in occasion of celebrating Raya</h2>
+              <h2 className="font-sans mt-6 text-xl font-bold">SUNDAY, 28 APRIL 2024</h2>
+              <p className='font-sans mt-3 text-md font-bold'>12-5PM</p>
+              <p className="font-sans mt-3 text-md font-bold">No.8, Lorong Berlian 5, Taman Batu Tiga, 40150 Shah Alam, Selangor</p>
+              <a href='/Submit' className='w-full max-w-md mt-6 inline-block rounded-lg border border-neutral-700 bg-yellow-600 px-5 py-4 font-sans transition-colors hover:border-gray-300 hover:bg-yellow-100 dark:hover:border-neutral-100 dark:hover:bg-yellow-600 dark:hover:bg-opacity-30'>
+                RSVP NOW
+              </a>
+            </div>
 
-			<h2 className="font-sans mt-10 text-sm text-center">We invite you to our open house in occasion in celebrating raya</h2>
-		
+          </div>
+
+          <div className="fixed bottom-0 left-0 right-0 flex h-24 w-full items-end justify-center bg-gradient-to-t from-teal-800 via-teal-950 to-transparent dark:from-teal-950 dark:via-teal-950 lg:relative lg:h-auto lg:w-auto lg:bg-none">
+            <a
+              className="flex items-center gap-2 p-8 lg:p-0"
+              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Powered by{' '}
+              <Image
+                src="/vercel.svg"
+                alt="Vercel Logo"
+                width={100}
+                height={20}
+                priority
+                className="dark:invert"
+              />
+            </a>
+          </div>
+        
+        </div>
       </div>
-
-		
-		{/* Event Information */}
-		<div className='relative flex flex-col text-center lg:max-w-lg lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-center'>
-
-			<h2 className="font-sans mb-3 text-xl text-center font-bold">SUNDAY, 28 APRIL 2024</h2>
-			<p className='font-sans mb-3 text-sm text-center font-bold'>12-5PM</p>
-			<p className="font-sans mb-3 text-sm text-center font-bold">No.8, Lorong Berlian 5, Taman Batu Tiga, 40150 Shah Alam, Selangor</p>
-			
-			<br/>
-
-			<a href='/Submit' className='group font-sans border-neutral-700 bg-yellow-600 rounded-lg border px-5 py-4 transition-colors
-			hover:border-gray-300
-			hover:bg-yellow-100 hover:dark:border-neutral-100 hover:dark:bg-yellow-600 hover:dark:bg-opacity-30'>RSVP NOW</a>
-		</div>
-		</div>
-		</main>
-	);
+    </main>
+  );
 }
