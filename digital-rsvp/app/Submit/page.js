c5dee83c67a03769654383aea32a930200fdd0fb
aesha98 <sityasya@gmail.com>
2024-04-17 00:14:10 +0800

    update form page

--- digital-rsvp/app/Submit/page.js
+++ digital-rsvp/app/Submit/page.js
@@ -1,7 +1,30 @@
+'use client';
+
 import Image from 'next/image'
+import { useState } from 'react';
+import { useFormState } from "react-dom";
 
 export default function Submit_rsvp()
 {
+	const [willAttend, setWillAttend] = useState(false);
+	const [guestCount, setGuestCount] = useState(1);
+
+	const handleAttendanceChange = (event) =>{
+		setWillAttend(event.target.checked);
+		//if user uncheck 'will attend' reset guest count and hide 
+		if(!event.target.checked){
+			setGuestCount(1);
+		}
+	};
+
+	const handGuestCountChange = (event) =>{
+		setGuestCount(event.target.value);
+	}
+
+	const handleSubmit = (event) => {
+    event.preventDefault();
+    // Submit form data
+  };
 	return(
 		<main className="relative flex flex-col bg-[url(/grid.svg)] p-10 [mask-image:linear-gradient(180deg, white,rgba(255,255,255,0))] items-center">
 			<div className='relative flex min-h-screen flex-col border border-yellow-400 overflow-hidden bg-teal-950 py-6 sm:py-12 p-10'>
@@ -33,21 +56,63 @@ export default function Submit_rsvp()
 				
 			</div>
 	
-		<div className='flex flex-col text-center lg:max-w-lg lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-center align-top justify-start'>
+		<div className='w-full flex flex-col text-center lg:max-w-lg lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-center align-top justify-start'>
 			{/* RSVP Form */}
-          <form className="mt-10 w-full flex flex-col items-center justify-center gap-6">
+
+          <form className="mt-10 w-full flex flex-col gap-6" >
             {/* Name Input */}
-            <input className="w-full max-w-md rounded-lg border px-5 py-3" type="text" placeholder="Your Name" required />
-            {/* Email Input */}
-            <input className="w-full max-w-md rounded-lg border px-5 py-3" type="email" placeholder="Email Address" required />
-			{/* Phone Number */}
-            <input className="w-full max-w-md rounded-lg border px-5 py-3" type="phone" placeholder="Phone Number" required />
+			<div className='mt-2 flex flex-col left-0 align-baseline'>
+			<label className='text-sm font-medium text-gray-900 dark:text-gray-300  font-sans text-left mb-4'>Name</label>
+            <input className="w-full max-w-md rounded-lg border px-5 py-2 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" placeholder="Name" required />
+			</div>
+
+			
+           {/* Tick Attendance Input */}
+		 		<label className='mt-4 text-sm font-medium text-gray-900 dark:text-gray-300  font-sans text-left'>Will you attend?</label>
+			<div className='flex'>
+			
+				<div className="flex items-center me-4">
+					
+					<input id="inline-checkbox" type="checkbox" value="" className="w-4 h-4 text-green-600 bg-gray-100 border-gray-300 rounded focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" checked={willAttend} onChange={handleAttendanceChange}/>
+					<label for="inline-checkbox" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300 font-sans">Yes, I will attend</label>
+				</div>
+				<div className="flex items-center me-4">
+					<input id="inline-2-checkbox" type="checkbox" value="" className="w-4 h-4 text-red-600 bg-gray-100 border-gray-300 rounded focus:ring-red-500 dark:focus:ring-red-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+					<label for="inline-2-checkbox" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300 font-sans">Sorry, unable to attend</label>
+				</div>
+				
+</div>
             {/* Number of Guests Input */}
-            <input className="w-full max-w-md rounded-lg border px-5 py-3" type="number" placeholder="Number of Guests (including you)" min="1" required />
+            
+	<div className='mt-4 flex flex-col left-0 align-baseline'>
+			<label className='text-sm text-gray-900 dark:text-gray-300  font-sans text-left mb-4'>Number Of Guests </label>
+             
+    <div className="relative flex items-center max-w-[11rem]">
+        <button type="button" id="decrement-button" data-input-counter-decrement="bedrooms-input" className="bg-gray-100 dark:bg-gray-100 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
+			
+            <svg className="w-3 h-3 text-gray-900 dark:text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h16"/>
+            </svg>
+        </button>
+        <input type="text" id="bedrooms-input" data-input-counter data-input-counter-min="1" data-input-counter-max="5" aria-describedby="helper-text-explanation" class="bg-gray-50 border-x-0 border-gray-100 h-11 font-medium text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full pb-6 dark:bg-teal-800 dark:border-gray-100 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" value="3" required />
+        <div className="absolute bottom-1 start-1/2 -translate-x-1/2 rtl:translate-x-1/2 flex items-center text-xs text-gray-400 space-x-1 rtl:space-x-reverse">
+            <svg className="w-2.5 h-2.5 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 8v10a1 1 0 0 0 1 1h4v-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v5h4a1 1 0 0 0 1-1V8M1 10l9-9 9 9"/>
+            </svg>
+            <span>People</span>
+        </div>
+        <button type="button" id="increment-button" data-input-counter-increment="bedrooms-input" className="bg-gray-100 dark:bg-gray-100 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
+            <svg className="w-3 h-3 text-gray-900 dark:text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16"/>
+            </svg>
+        </button>
+    </div>
+    <p id="helper-text-explanation" className="mt-2 text-sm text-gray-500 dark:text-gray-500 font-sans text-left">Please select the number of guest.</p>
+			</div>
           
             
 			{/* Submit Button */}     
-				<a href='/Submit' className='w-full max-w-md group font-sans border-neutral-700 bg-yellow-600 rounded-lg border px-5 py-4 transition-colors
+				<a href='/Submit' type='submit' className='w-full max-w-md group font-sans border-neutral-700 bg-yellow-600 rounded-lg border px-5 py-4 transition-colors
 			hover:border-gray-300
 			hover:bg-yellow-100 hover:dark:border-neutral-100 hover:dark:bg-yellow-600 hover:dark:bg-opacity-30'>Submit</a>
 			</form>
